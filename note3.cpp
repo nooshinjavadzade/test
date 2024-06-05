ffec2b75826fd01bf52efd1d049fa8070e5ba206
nooshinjavadzade <152793587+nooshinjavadzade@users.noreply.github.com>
2024-06-05 22:52:10 +0330

    Create note3.cpp
    some usual note

---
+++ note3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+using namespace std;
+int main() {
+    char x;
+    cin >>x;
+    if (isdigit(x))
+    {
+        cout << "yeeeeee";
+    }
+    else
+    {
+        cout << "ohhh";
+    }
+    //char in digit
+    
+    return 0;
+}
