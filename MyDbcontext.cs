858306f57c07ec0f1fb7725a9406c857719d364b
nooshinjavadzade <152793587+nooshinjavadzade@users.noreply.github.com>
2025-06-30 10:57:05 +0330

    Add files via upload

---
+++ MyDbcontext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Models;
+namespace Store.Data
+{
+    public class MyDbcontext:DbContext
+    {
+        public MyDbcontext(DbContextOptions<MyDbcontext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+    }
+}
