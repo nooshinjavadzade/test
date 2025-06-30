813a59c801359a839e3d6333a7dc242e73db8272
nooshinjavadzade <152793587+nooshinjavadzade@users.noreply.github.com>
2025-06-30 10:57:59 +0330

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Store.Models
+
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public ICollection<Orders>? Orders { get; set; } 
+    }
+}
